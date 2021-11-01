da7642a52a8cd802f5364e0b1e842829bde06638
denizgoez <denizgoez@gmail.com>
2021-11-01 15:43:01 +0100

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 if __name__ == "__main__":
     #Define file path
     webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
-    image_path = os.path.normpath(os.getcwd()+"\\photos_min_res_200")
+    image_path = os.path.normpath(os.getcwd()+"\\mercedes-benz-1-nov")
 
     #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
     # 'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018' done
@@ -29,14 +29,24 @@ if __name__ == "__main__":
     # 'Mercedes-Benz GLS-Class 2006', 'Mercedes-Benz GLS-Class 2012', 'Mercedes-Benz GLS-Class 2019' 
     # 'Mercedes-Benz GLE-Class 2005', 'Mercedes-Benz GLE-Class 2011', 'Mercedes-Benz GLE-Class 2019' 
     search_keys= [
-                  'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018',
-                  'Mercedes-Benz B-Class 2005', 'Mercedes-Benz B-Class 2011', 'Mercedes-Benz B-Class 2019' 
+                #  'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018',
+                #  'Mercedes-Benz B-Class 2005', 'Mercedes-Benz B-Class 2011', 'Mercedes-Benz B-Class 2019',
+                   'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2014', 'Mercedes-Benz C-Class 2021',
+                   'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz CLA-Class 2019',
+                   'Mercedes-Benz CLS-Class 2004', 'Mercedes-Benz CLS-Class 2011', 'Mercedes-Benz CLS-Class 2018' , 
+                   'Mercedes-Benz E-Class 2002', 'Mercedes-Benz E-Class 2009', 'Mercedes-Benz E-Class 2016', 
+                   'Mercedes-Benz G-Class 1990', 'Mercedes-Benz G-Class 2017', 
+                   'Mercedes-Benz S-Class 2005', 'Mercedes-Benz S-Class 2013', 'Mercedes-Benz S-Class 2020' ,
+                   'Mercedes-Benz Sprinter 2006', 'Mercedes-Benz A-Class 2018',
+                   'Mercedes-Benz V-Class 2003', 'Mercedes-Benz A-Class 2014', 
+                   'Mercedes-Benz GLS-Class 2006', 'Mercedes-Benz GLS-Class 2012', 'Mercedes-Benz GLS-Class 2019', 
+                   'Mercedes-Benz GLE-Class 2005', 'Mercedes-Benz GLE-Class 2011', 'Mercedes-Benz GLE-Class 2019'  
     ]
 
     #Parameters
-    number_of_images = 200
+    number_of_images = 300
     headless = False
-    min_resolution=(200,200)
+    min_resolution=(0,0)
     max_resolution=(9999,9999)
 
     #Main program
