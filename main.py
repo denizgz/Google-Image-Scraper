45fb747db5fec69b2ec599c3ec25f992732148ea
denizgoez <denizgoez@gmail.com>
2021-11-03 11:34:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,35 +13,25 @@ import os
 if __name__ == "__main__":
     #Define file path
     webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
-    image_path = os.path.normpath(os.getcwd()+"\\mercedes-benz-1-nov")
+    image_path = os.path.normpath(os.getcwd()+"\\mercedes-benz-3-nov")
 
-    #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    # 'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018' done
-    # 'Mercedes-Benz B-Class 2005', 'Mercedes-Benz B-Class 2011', 'Mercedes-Benz B-Class 2019' done
-    # 'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2014', 'Mercedes-Benz C-Class 2021' done
-    # 'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz CLA-Class 2019' done but less data
-    # 'Mercedes-Benz CLS-Class 2004', 'Mercedes-Benz CLS-Class 2011', 'Mercedes-Benz CLS-Class 2018'  
-    # 'Mercedes-Benz E-Class 2002', 'Mercedes-Benz E-Class 2009', 'Mercedes-Benz E-Class 2016' 
-    # 'Mercedes-Benz G-Class 1990', 'Mercedes-Benz G-Class 2017' 
-    # 'Mercedes-Benz S-Class 2005', 'Mercedes-Benz S-Class 2013', 'Mercedes-Benz S-Class 2020' 
-    # 'Mercedes-Benz Sprinter 2006', 'Mercedes-Benz A-Class 2018'
-    # 'Mercedes-Benz V-Class 2003', 'Mercedes-Benz A-Class 2014' 
-    # 'Mercedes-Benz GLS-Class 2006', 'Mercedes-Benz GLS-Class 2012', 'Mercedes-Benz GLS-Class 2019' 
-    # 'Mercedes-Benz GLE-Class 2005', 'Mercedes-Benz GLE-Class 2011', 'Mercedes-Benz GLE-Class 2019' 
-    search_keys= [
-                #  'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018',
+    #Add new search key into array 
+
+                # 'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018',
                 #  'Mercedes-Benz B-Class 2005', 'Mercedes-Benz B-Class 2011', 'Mercedes-Benz B-Class 2019',
-                   'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2014', 'Mercedes-Benz C-Class 2021',
-                   'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz CLA-Class 2019',
-                   'Mercedes-Benz CLS-Class 2004', 'Mercedes-Benz CLS-Class 2011', 'Mercedes-Benz CLS-Class 2018' , 
-                   'Mercedes-Benz E-Class 2002', 'Mercedes-Benz E-Class 2009', 'Mercedes-Benz E-Class 2016', 
-                   'Mercedes-Benz G-Class 1990', 'Mercedes-Benz G-Class 2017', 
-                   'Mercedes-Benz S-Class 2005', 'Mercedes-Benz S-Class 2013', 'Mercedes-Benz S-Class 2020' ,
-                   'Mercedes-Benz Sprinter 2006', 'Mercedes-Benz A-Class 2018',
-                   'Mercedes-Benz V-Class 2003', 'Mercedes-Benz A-Class 2014', 
-                   'Mercedes-Benz GLS-Class 2006', 'Mercedes-Benz GLS-Class 2012', 'Mercedes-Benz GLS-Class 2019', 
-                   'Mercedes-Benz GLE-Class 2005', 'Mercedes-Benz GLE-Class 2011', 'Mercedes-Benz GLE-Class 2019'  
-    ]
+                #    'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2014', 'Mercedes-Benz C-Class 2021',
+                #    'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz CLA-Class 2019',
+                #    'Mercedes-Benz CLS-Class 2004', 'Mercedes-Benz CLS-Class 2011', 'Mercedes-Benz CLS-Class 2018' , 
+                #    'Mercedes-Benz E-Class 2002', 'Mercedes-Benz E-Class 2009', 'Mercedes-Benz E-Class 2016', 
+                #    'Mercedes-Benz G-Class 1990', 'Mercedes-Benz G-Class 2017', 
+                #    'Mercedes-Benz S-Class 2005', 'Mercedes-Benz S-Class 2013', 'Mercedes-Benz S-Class 2020' ,
+                #    'Mercedes-Benz Sprinter 2006', 'Mercedes-Benz Sprinter-Class 2018',
+                #    'Mercedes-Benz V-Class 2003', 'Mercedes-Benz V-Class 2014', 
+                #    'Mercedes-Benz GLS-Class 2006', 'Mercedes-Benz GLS-Class 2012', 'Mercedes-Benz GLS-Class 2019', 
+                #    'Mercedes-Benz GLE-Class 2005', 'Mercedes-Benz GLE-Class 2011', 'Mercedes-Benz GLE-Class 2019 
+    search_keys= [
+               
+                'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2014', 'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz E-Class 2009']
 
     #Parameters
     number_of_images = 300
