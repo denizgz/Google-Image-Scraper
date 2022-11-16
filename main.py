d26a912b02ede808b0cd4001c58d119ce8ead9c4
denizgoez <denizgoez@gmail.com>
2022-11-16 16:35:23 +0100

    update

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 
 # import libraries
-from GoogleImage import GoogleImage
+from GoogleImage import GoogleImageScraper
 import os
 
 
 if __name__ == "__main__":
     # paths for driver
     webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
-    image_path = os.path.normpath(os.getcwd()+"\\mercedes-benz-okt")
+    image_path = os.path.normpath(os.getcwd()+"\\mercedes-benz-15112022-2")
 
     #Add new search key into array 
     search_keys= [
-                'Mercedes-Benz A-Class 2004', 'Mercedes-Benz A-Class 2012', 'Mercedes-Benz A-Class 2018',
-                 'Mercedes-Benz B-Class 2005', 'Mercedes-Benz B-Class 2011', 'Mercedes-Benz B-Class 2019',
-                   'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2014', 'Mercedes-Benz C-Class 2021',
-                   'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz CLA-Class 2019',
-                   'Mercedes-Benz CLS-Class 2004', 'Mercedes-Benz CLS-Class 2011', 'Mercedes-Benz CLS-Class 2018' , 
-                   'Mercedes-Benz E-Class 2002', 'Mercedes-Benz E-Class 2009', 'Mercedes-Benz E-Class 2016', 
-                   'Mercedes-Benz G-Class 1990', 'Mercedes-Benz G-Class 2017', 
-                   'Mercedes-Benz S-Class 2005', 'Mercedes-Benz S-Class 2013', 'Mercedes-Benz S-Class 2020' ,
-                   'Mercedes-Benz Sprinter 2006', 'Mercedes-Benz Sprinter-Class 2018',
-                   'Mercedes-Benz V-Class 2003', 'Mercedes-Benz V-Class 2014', 
-                   'Mercedes-Benz GLS-Class 2006', 'Mercedes-Benz GLS-Class 2012', 'Mercedes-Benz GLS-Class 2019', 
-                   'Mercedes-Benz GLE-Class 2005', 'Mercedes-Benz GLE-Class 2011', 'Mercedes-Benz GLE-Class 2019 
-                'Mercedes-Benz C-Class 2007', 'Mercedes-Benz C-Class 2021', 'Mercedes-Benz CLA-Class 2013', 'Mercedes-Benz E-Class 2009']
+                # "A-Class",
+                # "B-Class",
+                # "C-Class",
+                # "CLA",
+                # "CLS",
+                # #"E-Class",
+                # "G-Class",
+                # "GLA",
+                # "GLB",
+                # "GLC",
+                # "GLE",
+                # "GLS",
+                # "S-Class",
+                # "SL",
+                # "V-Class",
+                # "EQA",
+                # "EQB",
+                # "EQC",
+                # "EQE",
+                # "EQS SUV",
+                # "EQS",
+                # "EQV",
+                #"T-Class",
+                #"smart",
+                #"AMG GT",
+                #"Sprinter",
+                #"Citan",
+                #"Vito",
+                "Marco Polo Mercedes-Benz"]
 
     # parameters
     number = 300
@@ -33,9 +49,9 @@ if __name__ == "__main__":
 
     # main program
     for search_key in search_keys:
-        image_scrapper = GoogleImage(webdriver_path,image_path,search_key,number,headless,min_res,max_res)
+        image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number,headless,min_res,max_res)
         image_urls = image_scrapper.find_image_urls()
-        image_scrapper.save_images(image_urls)
+        image_scrapper.save_images(image_urls, keep_filenames=False)
     
     # release resources    
     del image_scrapper
--- patch.py
+++ patch.py
@@ -1,79 +1,85 @@
-# import librarys 
-import zipfile
-import stat
-from sys import pform
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException, SessionNotCreatedException
 import sys
 import os
 import urllib.request
 import re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import zipfile
+import stat
+from sys import platform
 
-def download_lastest_cdriver(present_chrome_version=""):
-'''Find the latest chromedriver, download, (un)zip, set permissions to executable.'''
-    def get_pform_fname():
-        fname = ''
+def webdriver_executable():
+    if platform == "linux" or platform == "linux2" or platform == "darwin":
+        return 'chromedriver'
+    return 'chromedriver.exe'
+
+def download_lastest_chromedriver(current_chrome_version=""):
+    def get_platform_filename():
+        filename = ''
         is_64bits = sys.maxsize > 2**32
     
-        if pform == "linux" or pform == "linux2":
+        if platform == "linux" or platform == "linux2":
             # linux
-            fname += 'linux'
-            fname += '64' if is_64bits else '32'
-        elif pform == "win32":
-             # windows...
-            fname += 'win32'
-        elif pform == "darwin":
-            # os x
-            fname += 'mac64'
-        fname += '.zip'
-        return fname
+            filename += 'linux'
+            filename += '64' if is_64bits else '32'
+        elif platform == "darwin":
+            # OS X
+            filename += 'mac64'
+        elif platform == "win32":
+            # Windows...
+            filename += 'win32'
     
-    res = False
-
+        filename += '.zip'
+    
+        return filename
+    
+    # Find the latest chromedriver, download, unzip, set permissions to executable.
+    
+    result = False
     try:
         url = 'https://chromedriver.chromium.org/downloads'
         base_driver_url = 'https://chromedriver.storage.googleapis.com/'
-        file_name = 'chromedriver_' + get_pform_fname()
+        file_name = 'chromedriver_' + get_platform_filename()
         pattern = 'https://.*?path=(\d+\.\d+\.\d+\.\d+)'
     
-        # latest cdriver
+        # Download latest chromedriver.
         stream = urllib.request.urlopen(url)
-        cont = stream.read().decode('utf8')
+        content = stream.read().decode('utf8')
     
-        # parse the latest vers.
-        every_match = re.findall(pattern, cont)
+        # Parse the latest version.
+        all_match = re.findall(pattern, content)
         
-        if every_match:
-            if(present_chrome_version!=""):
-                print("information! cdriver update")
-                every_match = list(set(re.findall(pattern, cont)))
-                present_chrome_version = ".".join(present_chrome_version.split(".")[:-1])
-                version_match = [i for i in every_match if re.search("^%s"%present_chrome_version,i)]
+        if all_match:
+            # Version of latest driver.
+            if(current_chrome_version!=""):
+                print("[INFO] updating chromedriver")
+                all_match = list(set(re.findall(pattern, content)))
+                current_chrome_version = ".".join(current_chrome_version.split(".")[:-1])
+                version_match = [i for i in all_match if re.search("^%s"%current_chrome_version,i)]
                 version = version_match[0]
             else:
-                print("information! installing newest cdriver")
-                version = every_match[1]
+                print("[INFO] installing new chromedriver")
+                version = all_match[1]
             driver_url = base_driver_url + version + '/' + file_name
     
-            # download file
-            print('information - this can take a minute! downloading chromedriver ver: %s: %s'% (version, driver_url))
-            application_path = os.path.dirname(os.path.realpath(__file__))
-            cdriver_path = os.path.normpath(application_path+"\\webdriver\\chromedriver.exe")
-            f_path = os.path.normpath(application_path + '\\webdriver\\' + file_name)
-            urllib.request.urlretrieve(driver_url, f_path)
+            # Download the file.
+            print('[INFO] downloading chromedriver ver: %s: %s'% (version, driver_url))
+            app_path = os.path.dirname(os.path.realpath(__file__))
+            chromedriver_path = os.path.normpath(os.path.join(app_path, 'webdriver', webdriver_executable()))
+            file_path = os.path.normpath(os.path.join(app_path, 'webdriver', file_name))
+            urllib.request.urlretrieve(driver_url, file_path)
     
-            # folder unzip
-            with zipfile.ZipFile(f_path, 'r') as zip_ref:
-                zip_ref.extractall(os.path.normpath(application_path + '\\webdriver\\'))
-    
-            st = os.stat(cdriver_path)
-            os.chmod(cdriver_path, st.st_mode | stat.S_IEXEC)
-            print('information! lastest cdriver downloaded')
-            # tidy up
-            os.remove(f_path)
-            res = True
+            # Unzip the file into folder
+            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                zip_ref.extractall(os.path.normpath(os.path.join(app_path, 'webdriver')))
+            st = os.stat(chromedriver_path)
+            os.chmod(chromedriver_path, st.st_mode | stat.S_IEXEC)
+            print('[INFO] lastest chromedriver downloaded')
+            # Cleanup.
+            os.remove(file_path)
+            result = True
     except Exception:
-        print("[WARN] unable to download cdriver. the system use local vers.")
+        print("[WARN] unable to download lastest chromedriver. the system will use the local version instead.")
     
-    return res
-
+    return result
